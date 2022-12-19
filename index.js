e83427bb80c52784ac5586b742a8ddfbde8ab4af
Rawan <rawnq.sb@gmail.com>
2022-12-19 21:38:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+//let companyName = 'Scuber';
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea'
+//const mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo= 'Susan Smith';
+//const  companyCeo = 'Susan Smith';
+
